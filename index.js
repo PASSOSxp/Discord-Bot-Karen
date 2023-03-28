b3eac0c78fa414b824cd25d01a92401f54d81c3f
Gabriel Passolongo <102549945+gabrielpassos7@users.noreply.github.com>
2023-03-28 16:07:59 +0000

    KAREN 2.0

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 /* Mantendo o bot online */
 
 const express = require('express');
@@ -13,113 +14,158 @@ app.listen(process.env.PORT);
 
 /* Conectando o bot ao Discord */
 
-const Discord = require("discord.js")
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+require('dotenv').config()
+const Discord = require('discord.js');
+const fs = require('fs');
 
-client.on("ready", () => {
+const client = new Discord.Client({
+  intents: [
+    'GUILDS',            // Permite acessar informações de servidores
+    'GUILD_MESSAGES'     // Permite acessar mensagens e eventos em servidores
+  ]
+});
+
+const moment = require('moment-timezone')
+
+client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-
-// at the top of your file
-const { MessageEmbed } = require('discord.js');
-
-
-/* Agenda */
-
-var comp1 = ""
-var comp2 = ""
-var comp3 = ""
+function sendMessage(message) {
+  const channel = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID)
+  channel.send(message)
+}
 
 
-/* Prefixo do bot */
+const commands = {
+  add: addAppointment,
+  help: showHelp,
+  list: listAppointments,
+  clear: clearAppointments
+};
 
-const prefix = "!k "
+const help = {
+  add: '!add [data no formato YYYY-MM-DD] [hora no formato HH:mm] [descrição do compromisso] - Adiciona um compromisso na agenda',
+  help: '!help - Exibe esta mensagem de ajuda',
+  list: '!list - Lista os compromissos agendados',
+  clear: '!clear - Limpa todos os compromissos agendados'
+}
 
+function showHelp(message) {
+  const commands = Object.keys(help).map((command) => help[command])
+  message.reply(`Comandos disponíveis:\n${commands.join('\n')}`)
+}
 
-/* Mensagens */
+const agenda = {}
 
-client.on("messageCreate", msg => {
-
-  
-  /* Ignoradas */
-
-  if (msg.author.bot) return
-
-  
-  /* Executadas */
-  
-  if (msg.content ===  prefix + "help"){
-    // inside a command, event listener, etc.
-    const exampleEmbed = new MessageEmbed()
-      .setColor('#0099ff')
-      .setTitle('Você precisa de ajuda?')
-      .setURL('https://github.com/PASSOSxp')
-      .setAuthor({ name: `${client.user.tag}`, iconURL: client.user.displayAvatarURL(), url: 'https://github.com/PASSOSxp' })
-      .setDescription('Principais comandos:')
-      .setThumbnail(client.user.displayAvatarURL())
-      .addFields(
-        { name: prefix + 'agenda', value: 'Exibe o que há na Agenda' },
-        { name: prefix + 'agendar', value: 'Marca algo na Agenda' },
-        { name: prefix + 'resetagenda', value: 'Reseta a Agenda' },
-        { name: prefix + 'play', value: 'Toca uma música' },
-        { name: prefix + 'stop', value: 'Para a música' },
-        { name: prefix + 'skip', value: 'Pula a música' },
-        { name: prefix + 'help', value: 'Exibe todos os comandos' },
-        { name: '\u200B', value: '\u200B' }/*,
-        { name: 'Inline field title', value: 'Some value here', inline: true },
-        { name: 'Inline field title', value: 'Some value here', inline: true },
-      */)
-      /* .addField('Inline field title', 'Some value here', true)
-      .setImage('') */
-      .setTimestamp()
-      .setFooter({ text: `${client.user.tag}`, iconURL: client.user.displayAvatarURL() });
-    msg.channel.send({ embeds: [exampleEmbed] });
+function listAppointments(message) {
+  const appointmentList = getAppointments();
+  if (appointmentList.length > 0) {
+    const appointmentListFormatted = appointmentList.map((appointment) => {
+      const date = moment.tz(appointment.date, 'x', 'America/Sao_Paulo');
+      return `${date.format('DD/MM/YYYY HH:mm')} - ${appointment.description}`;
+    });
+    const reply = `Compromissos agendados:\n${appointmentListFormatted.join('\n')}`;
+    message.reply(reply);
+  } else {
+    message.reply('Não há compromissos agendados.');
   }
-  else if (msg.content.startsWith(prefix + "agendar")) {
+}
 
-    if (comp1 == "") {
-      comp1 = msg.content.slice(10).split()
+client.on('message', (message) => {
+  if (message.content.startsWith('!add')) {
+    const args = message.content.split(' ')
+    const date = moment.tz(`${args[1]} ${args[2]}`, 'YYYY-MM-DD HH:mm', 'America/Sao_Paulo')
+    const description = args.slice(3).join(' ')
+    const now = moment();
+    if (date.isBefore(now)) {
+      message.reply('A data e hora do compromisso devem estar no futuro.');
+      return;
     }
-    else if (comp2 == "") {
-      comp2 = msg.content.slice(10).split()
-    }
-    else if (comp3 == "") {
-      comp3 = msg.content.slice(10).split()
-    }
-    else {
-      msg.channel.send("Agenda cheia!")
-    }
-
-  }
-  else if (msg.content ===  prefix + "agenda") {
-
-    if (comp1 == "" && comp2 == "" && comp3 == "") {
-      msg.channel.send("Agenda vazia!")
-    }
-    else {
-      var agenda = "AGENDA\n" + comp1 +"\n"+ comp2 +"\n"+ comp3
-    
-    msg.channel.send(agenda)
-    }
-  }
-    else if (msg.content === prefix + "resetagenda") {
-      comp1 = ""
-      comp2 = ""
-      comp3 = ""
-    }
-  else if (msg.content ===  prefix + "play") {
-    msg.channel.send("play")
-  }
-  else if (msg.content ===  prefix + "stop") {
-    msg.channel.send("stop")
-  }
-  else if (msg.content ===  prefix + "skip") {
-    msg.channel.send("skip")
+    addAppointment(date, description);
+    message.reply(`Compromisso agendado para ${date.format('DD/MM/YYYY HH:mm')} com a descrição "${description}".`)
+  } else if (message.content.startsWith('!help')) {
+    const commands = Object.keys(help).map((command) => help[command])
+    message.reply(`Comandos disponíveis:\n${commands.join('\n')}`)
+  } else if (message.content.startsWith('!list')) {
+    commands.list(message);
+  } else if (message.content.startsWith('!clear')) {
+    clearAppointments(message);
   }
 })
 
-          
+
+function checkAgenda() {
+  const now = moment.tz('America/Sao_Paulo');
+  const oneDayFromNow = moment.tz('America/Sao_Paulo').add(1, 'day');
+  let appointmentList = getAppointments();
+  const notifiedAppointments = [];
+
+  appointmentList = appointmentList.filter((appointment) => {
+    const date = moment.tz(appointment.date, 'x', 'America/Sao_Paulo');
+    if (date.isBefore(now)) {
+      // O compromisso já passou, então é removido da lista
+      return false;
+    }
+    return true;
+  });
+
+  appointmentList.forEach((appointment) => {
+    const date = moment.tz(appointment.date, 'x', 'America/Sao_Paulo');
+    if (date.isBetween(now, oneDayFromNow)) {
+      const description = appointment.description;
+      if (!appointment.notified) {
+        sendMessage(`Lembrete: Amanhã (${date.format('DD/MM/YYYY HH:mm')}) você tem o seguinte compromisso: ${description}`);
+        notifiedAppointments.push({ ...appointment, notified: true });
+      } else {
+        notifiedAppointments.push(appointment);
+      }
+    } else {
+      notifiedAppointments.push(appointment);
+    }
+  });
+
+  saveAppointments(notifiedAppointments);
+}
+
+
+function getAppointments() {
+  let appointments = [];
+  if (fs.existsSync('appointments.json') && fs.statSync('appointments.json').size > 0) { // verifica se o arquivo existe e não está vazio
+    appointments = JSON.parse(fs.readFileSync('appointments.json')); // lê o arquivo JSON
+  }
+  return appointments;
+}
+
+function addAppointment(date, description) {
+  let appointments = getAppointments();
+  appointments.push({ date: date.valueOf(), description, notified: false });
+  saveAppointments(appointments);
+  agenda[date.valueOf()] = description;
+}
+
+function saveAppointments(appointments) {
+  fs.writeFileSync('appointments.json', JSON.stringify(appointments));
+}
+
+function clearAppointments(message) {
+  saveAppointments([]);
+  Object.keys(agenda).forEach((key) => delete agenda[key]);
+  message.reply('Todos os compromissos agendados foram apagados.');
+}
+
+
+const now = moment();
+if (now.isBefore(now)) {
+  message.reply('A data e hora do compromisso devem estar no futuro.');
+  return;
+}
+
+
+setInterval(checkAgenda, 60 * 1000) // Verificar a agenda a cada hora
+
+
+
 /* Token */
 
 client.login(process.env['TOKEN'])
